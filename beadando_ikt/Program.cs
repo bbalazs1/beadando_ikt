012ddf916d94e983f9dd1aeb49876683c06f854c
bbalazs1 <b3ligbal@vasvari.org>
2025-10-19 22:18:58 +0200

    bekeresek a felhasznalotol

--- beadando_ikt/Program.cs
+++ beadando_ikt/Program.cs
@@ -10,7 +10,23 @@ namespace beadando_ikt
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Add meg a nevedet (Vezetéknév Keresztnev): ");
+            string nev = Console.ReadLine();
 
+
+            double testsuly = 0;
+            while (true)
+            {
+                Console.WriteLine("Add meg a testsúlyod (50 - 120 kg): ");
+                if (double.TryParse(Console.ReadLine(), out testsuly) && testsuly >= 50 && testsuly <= 120)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Hibás érték, 50 és 120 között adj meg egy számot!");
+                }
+            }
         }
     }
 }
