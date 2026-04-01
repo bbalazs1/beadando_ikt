bf4563d7a0dd25ff66f3ac4d5b80d0ed7150bd59
bbalazs1 <b3ligbal@vasvari.org>
2026-04-01 22:37:08 +0200

    gui elkeszitese

--- beadando_ikt/Program.cs
+++ beadando_ikt/Program.cs
@@ -7,5 +7,34 @@ namespace BankiApp
     class Program
     {
         static List<Account> szamlak = new List<Account>();
+        static void Main(string[] args)
+        {
+            AdatokBetoltese();
+            string valasztas = "";
+
+            while (valasztas != "0")
+            {
+                Console.Clear();
+                Console.WriteLine("=== EGYSZERŰ BANKI RENDSZER ===");
+                Console.WriteLine("1. Befizetés");
+                Console.WriteLine("2. Kifizetés");
+                Console.WriteLine("3. Utalás két számla között");
+                Console.WriteLine("4. Hitelkeret módosítása");
+                Console.WriteLine("5. Napló mentése (számlánként)");
+                Console.WriteLine("6. Összes egyenleg megtekintése");
+                Console.WriteLine("0. Kilépés");
+                Console.Write("\nVálasszon menüpontot: ");
+                valasztas = Console.ReadLine();
+
+                if (valasztas == "1") BefizetesMenu();
+                else if (valasztas == "2") KifizetesMenu();
+                else if (valasztas == "3") UtalasMenu();
+                else if (valasztas == "4") HitelkeretMenu();
+                else if (valasztas == "5") MentesMenu();
+                else if (valasztas == "6") ListazasMenu();
+            }
+        }
+
+   
 
         
